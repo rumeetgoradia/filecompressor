8205dd9a54f63eae672d81290fda8fe3ec63d487
Rumeet U. Goradia <rumeet.goradia@gmail.com>
2019-04-01 00:06:27 -0400

    Finalize comments

--- fcdatastructs.c
+++ fcdatastructs.c
@@ -8,13 +8,7 @@
 #include<stdio.h>
 #include "fcdatastructs.h"
 
-void print_list(llist_node * root) {
-	llist_node * ptr = root;
-	for (ptr = root; ptr != NULL; ptr = ptr->next) {
-		printf("%s : %d\n", ptr->token, ptr->freq);
-	}
-}
-
+/* Create a node for Huffman tree using parameters */
 tree_node * create_node(char * token, unsigned int freq) {	
 	tree_node * ptr = (tree_node *)malloc(sizeof(tree_node));
 	ptr->left = NULL;
@@ -25,6 +19,7 @@ tree_node * create_node(char * token, unsigned int freq) {
 	return ptr;
 }
 
+/* Initializes minheap tree */
 tree * create_tree(unsigned int max_size) {
 	tree * ptr = (tree *)malloc(sizeof(tree));
 	ptr->size = 0;
@@ -32,13 +27,14 @@ tree * create_tree(unsigned int max_size) {
 	ptr->node_arr = (tree_node **)malloc(max_size * sizeof(tree_node *));
 	return ptr;
 }
-
+/* Utility function that swaps tree_node lists at specific index */
 void swap_nodes(tree_node ** first, tree_node ** second) {
 	tree_node * temp = *first;
 	*first = *second;
 	*second = temp;
 }
 
+/* Heapify function based on algorithm */
 void heapify(tree * root, int index) {
 	int base = index;
 	int left = 2 * index + 1;
@@ -56,6 +52,7 @@ void heapify(tree * root, int index) {
 	return;
 }
 
+/* Removes the minimum node in the tree and returns it */
 tree_node * remove_min(tree * root) {
 	tree_node * ptr = root->node_arr[0];
 	int i = 1;
@@ -65,6 +62,7 @@ tree_node * remove_min(tree * root) {
 	return ptr;
 }
 
+/* Adds node to the minheap */
 void insert_tree_node(tree * root, tree_node * node) {
 	++root->size;
 	int i = root->size - 1;
@@ -75,6 +73,7 @@ void insert_tree_node(tree * root, tree_node * node) {
 	root->node_arr[i] = node;
 }
 
+/* Converts heap into correctly sorted minheap */
 void build_tree(tree * root) {
 	int i = 0;
 	int j = root->size - 1;
@@ -84,6 +83,7 @@ void build_tree(tree * root) {
 
 }
 
+/* Initializes tree, sets up tree nodes, and organizes tree into minheap */
 tree * full_tree(unsigned int size, llist_node * node) {
 	tree * root = create_tree(size);
 	llist_node * ptr = node;
@@ -100,6 +100,7 @@ tree * full_tree(unsigned int size, llist_node * node) {
 	return root;
 }
 
+/* Algorithm to build Huffman tree from minheap */
 tree_node * build_huffman(unsigned int size, llist_node * node) {
 	tree_node * parent;
 	tree_node * left;
@@ -115,6 +116,7 @@ tree_node * build_huffman(unsigned int size, llist_node * node) {
 		if (right != NULL) {
 			rightfreq = right->freq;
 		}
+		/* Enter unique token for nodes that are technically trees */
 		char temp[3] = "/\\\0";
 		parent = create_node(temp, leftfreq + rightfreq);
 		parent->left = left;
@@ -124,19 +126,24 @@ tree_node * build_huffman(unsigned int size, llist_node * node) {
 	return remove_min(root);
 }
 
+/* Sets specific numbers within array and writes them to file once leaf is reached */
 void set_codes(tree_node * node, unsigned short code_arr[], int parent, int fd) {
+	/* If going left, set current node to 0 and recurse for left node */
 	if (node->left != NULL) {
 		code_arr[parent] = 0;
 		set_codes(node->left, code_arr, parent+1, fd);
 	}
+	/* If going right, set current index to 1 and recurse for right node */
 	if (node->right != NULL) {
 		code_arr[parent] = 1;
 		set_codes(node->right, code_arr, parent+1, fd);
 	}
+	/* If reached a leaf, write the values stored previously in array up to parent */
 	if (node->left == NULL && node->right == NULL) {
 		int i = 0;
 		char buf[parent];
 		int count = 0;
+		/* From 0 up to parent, write each number as char */
 		for (i = 0; i < parent; ++i) {
 			if (code_arr[i] == 1 || code_arr[i] == 0) {
 				buf[i] = code_arr[i] + '0';
@@ -145,6 +152,7 @@ void set_codes(tree_node * node, unsigned short code_arr[], int parent, int fd) 
 				break;
 			}
 		}
+		/* Then tab, token, new line */
 		write(fd, buf, count);
 		write(fd, "\t", 1);
 		write(fd, node->token, strlen(node->token));
@@ -152,6 +160,7 @@ void set_codes(tree_node * node, unsigned short code_arr[], int parent, int fd) 
 	}
 }
 
+/* Goes through entire Huffman tree building process */
 void huffman(int size, llist_node * root, int fd) {
 	tree_node * base = build_huffman(size, root);
 	short codes[size];
--- fcdatastructs.h
+++ fcdatastructs.h
@@ -3,6 +3,8 @@
 * as2811, rug5			 *
 * CS214: Section 04		 *
 **********************************/
+/* Using a minheap structure: tree_nodes are the individual values of the minheap and the tree
+ * is the minheap. Functions to optimize and arrange each of these are in fcdatastructs.c */
 typedef struct tree_node {
 	char * token;
 	unsigned int freq;
