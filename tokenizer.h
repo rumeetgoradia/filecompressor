c967406e10bd2848ebc3df5e80a48626a1792513
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-03-31 23:29:23 -0400

    Add header comment

--- tokenizer.h
+++ tokenizer.h
@@ -1,3 +1,8 @@
+/*********************************
+* Aditi Singh and Rumeet Goradia *
+* as2811, rug5                   *
+* CS214: Section 04              *
+**********************************/
 int count_codebook(char * input, int length);
 void compress(int fd, char * input, int length, char ** codes, char ** tokens, int size);
 void decompress(int fd, char * input, int length, char ** codes, char ** tokens, int size);
